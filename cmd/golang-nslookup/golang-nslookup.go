319b6649000aa8314407a071a833f61036389847
Andrew McDermott <aim@frobware.com>
2022-09-05 13:00:08 +0100

    Updates

--- cmd/golang-nslookup/golang-nslookup.go
+++ cmd/golang-nslookup/golang-nslookup.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/go-kit/log/level"
 	"github.com/pkg/errors"
-	"github.com/thanos-io/thanos/pkg/discovery/dns"
-
 	"github.com/prometheus/common/version"
 	"github.com/thanos-io/thanos/pkg/extkingpin"
 	"github.com/thanos-io/thanos/pkg/logging"
@@ -25,32 +21,14 @@ func main() {
 	interval := app.Flag("interval", "Interval between successive lookups.").Default("1s").Duration()
 	logFormat := app.Flag("log.format", "Log format to use. Possible options: logfmt or json.").Default(logging.LogFormatLogfmt).Enum(logging.LogFormatLogfmt, logging.LogFormatJSON)
 	logLevel := app.Flag("log.level", "Log filtering level.").Default("info").Enum("error", "warn", "info", "debug")
-	resolverName := app.Flag("resolver.name", "Resolver implementation.").Default("miekgdns").String()
+	resolverName := app.Flag("resolver.name", "Resolver implementation.").Default("unknown").String()
 
 	app.Parse()
 
 	logger := logging.NewLogger(*logLevel, *logFormat, *resolverName)
 
-	_, _, err := net.SplitHostPort(*address)
-	if err != nil && strings.Contains(err.Error(), "missing port in address") {
-		// thanos resolver implementation needs a port number.
-		*address = fmt.Sprintf("%s:53", *address)
-	} else if err != nil {
-		level.Error(logger).Log("err", errors.Wrapf(err, "split host port failed: %v", err))
-		os.Exit(1)
-	}
-
-	var resolver dns.Resolver
-
-	switch *resolverName {
-	case "miekgdns":
-		resolver = dns.NewResolver(dns.MiekgdnsResolverType.ToResolver(logger), logger)
-	default:
-		resolver = dns.NewResolver(dns.GolangResolverType.ToResolver(logger), logger)
-	}
-
 	for {
-		if values, err := resolver.Resolve(context.TODO(), *address, dns.A); err != nil {
+		if values, err := net.LookupHost(*address); err != nil {
 			level.Error(logger).Log("err", errors.Wrapf(err, "lookup failed: %v", err))
 		} else {
 			level.Info(logger).Log("records", fmt.Sprintf("%v", values))
